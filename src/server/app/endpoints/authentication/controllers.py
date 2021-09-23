5863088cafc2a87603404279cd4ddf9a018e8162
MatthiasRiener <matti.riener@gmail.com>
2021-09-23 20:43:56 +0200

    keycloak redirect geht hihi

--- src/server/app/endpoints/authentication/controllers.py
+++ src/server/app/endpoints/authentication/controllers.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Blueprint
-from ../../db/settings import oidc
+from ...db.settings import oidc
+
 
 authentication_page = Blueprint("authentication", __name__)
 
@@ -7,3 +8,13 @@ authentication_page = Blueprint("authentication", __name__)
 @oidc.require_login
 def logIn(): 
     print('hfuodsonfsjda')
+
+    user_creds = oidc.user_getinfo(
+        ['preferred_username', 'email', 'sub', 'roles'])
+        
+    user_id = user_creds.get('sub')
+    user_name = user_creds.get('preferred_username')
+    user_mail = user_creds.get('email')
+
+
+    return user_id + "-" + user_name + "-" + user_mail
