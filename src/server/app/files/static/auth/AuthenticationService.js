dadd8e17a6b5b86abeb6d32d16d509b63f5ecbc3
MatthiasRiener <matti.riener@gmail.com>
2021-09-27 16:13:41 +0200

    main push ups

--- src/server/app/files/static/auth/AuthenticationService.js
+++ src/server/app/files/static/auth/AuthenticationService.js
@@ -1,106 +1,99 @@
-class AuthenticationService {
-    constructor() {
-        console.log("AuthenticationService initialized.")
-        this.baseURL = "http://localhost:5000";
+baseURL = "http://localhost:5000";
+
+function sendRequestToServer(args) {
+
+    if ('isCallback' in args) {
+        ajaxRequest(args['resolve'], args['reject'], args)
+        return;
     }
-
-    sendRequestToServer(args) {
-
-        if('isCallback' in args) {
-            this.ajaxRequest(args['resolve'], args['reject'], args)
-            return;
-        } 
-        return new Promise(function(resolve, reject) {
-            this.ajaxRequest(resolve, reject, args);
-        })
-    }
+    return new Promise(function (resolve, reject) {
+        ajaxRequest(resolve, reject, args);
+    })
+}
 
 
-    ajaxRequest(resolve, reject, args) {
-        $.ajax({
-            type: args.type,
-            url: this.baseURL + args.url,
-            data: args.data == undefined ? {} : args.data,
-            headers: {
-                Authorization: "Bearer " + this.getAccessToken(),
+function ajaxRequest(resolve, reject, args) {
+    $.ajax({
+        type: args.type,
+        url: baseURL + args.url,
+        data: args.data == undefined ? {} : args.data,
+        headers: {
+            Authorization: "Bearer " + getAccessToken(),
+        },
+        statusCode: {
+            400: function () {
+                alert("400 status code! user error");
             },
-            statusCode: {
-                400: function () {
-                    alert("400 status code! user error");
-                },
-                401: function () {
-                    this.silentLogin(this.getRefreshToken(), this.sendRequestToServer, args, resolve, reject);
-                },
-                801: function() {
-                    console.log("801 Fehler.")
-                }
+            401: function () {
+                silentLogin(getRefreshToken(), sendRequestToServer, args, resolve, reject);
             },
-            success: function (data) {
-                resolve(JSON.parse(data));
-            },
-        })
-    }
+            801: function () {
+                console.log("801 Fehler.")
+            }
+        },
+        success: function (data) {
+            resolve(JSON.parse(data));
+        },
+    })
+}
 
-    silentLogin(callback, args, resolve, reject) {
-        $.ajax({
-            type: "POST",
-            url: baseURL + "/authentication/refresh_token",
-            headers: {
-            Authorization: "Bearer " + this.getRefreshToken(),
-            },
-            statusCode: {
+function silentLogin(callback, args, resolve, reject) {
+    $.ajax({
+        type: "POST",
+        url: baseURL + "/authentication/refresh_token",
+        headers: {
+            Authorization: "Bearer " + getRefreshToken(),
+        },
+        statusCode: {
             400: function () {
                 alert("400 status code! user error");
             },
             401: function () {
                 alert("500 status code! server error");
             },
-            },
-            success: function (data) {
-                args["isCallback"] = true;
-                args['resolve'] = resolve;
-                args['reject'] = reject;
-                this.setAccessToken(data);
-                callback(args);
-            },
-        });
-     }
-
-
-
-    setAccessToken(access) {
-        if(!this.checkToken(access)) {
-            return;
-        }
-
-        localStorage.setItem("access", access);
-    }
-
-    setRefreshToken(refresh) {
-        if(!this.checkToken(refresh)) {
-            return;
-        }
-
-        localStorage.setItem("refresh", refresh);
-    }
-
-    getAccessToken() {
-        return localStorage.getItem("access");
-    }
-
-    getRefreshToken() {
-        return localStorage.getItem("refresh");
-    }
-
-    checkToken(token) {
-        if(token == null || token == undefined || token === '') {
-            return false;
-        }
-        return true;
-    }
-
+        },
+        success: function (data) {
+            args["isCallback"] = true;
+            args['resolve'] = resolve;
+            args['reject'] = reject;
+            setAccessToken(data);
+            callback(args);
+        },
+    });
 }
 
 
-const aService = new AuthenticationService();
+
+function setAccessToken(access) {
+    if (checkToken(access)) {
+        return;
+    }
+
+    localStorage.setItem("access", access);
+}
+
+function setRefreshToken(refresh) {
+    if (checkToken(refresh)) {
+        return;
+    }
+
+    localStorage.setItem("refresh", refresh);
+}
+
+function getAccessToken() {
+    return localStorage.getItem("access");
+}
+
+function getRefreshToken() {
+    return localStorage.getItem("refresh");
+}
+
+function checkToken(token) {
+    if (token == null || token == undefined || token === '') {
+        return false;
+    }
+    return true;
+}
+
+
 
