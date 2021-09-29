5dd647c69b6bbbd72db42f62ae2f0caec5c8dad0
MatthiasRiener <matti.riener@gmail.com>
2021-09-29 18:28:01 +0200

    änderungen

--- src/server/app/files/static/profile/js/script.js
+++ src/server/app/files/static/profile/js/script.js
@@ -1,4 +1,29 @@
-$('.section-group-item').click(function() {
+
+
+
+$('.section-group-item').click(function () {
     $('.section-group-item').removeClass("active");
     $('.section-group-item').eq($(this).index()).addClass("active");
 });
+
+$('.section-group-item, .view-all-reviews-button').click(() => {
+    goToByScroll("reviews-section")
+});
+
+function goToByScroll(id) {
+
+    $('html,body').animate({
+        scrollTop: $("#" + id).offset().top - window.innerHeight / 100 * 5
+    },
+        'slow');
+}
+
+$('.scroll-button').click(function () { topFunction() });
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    $('html,body').animate({
+        scrollTop: 0
+    },
+        'slow');
+}
