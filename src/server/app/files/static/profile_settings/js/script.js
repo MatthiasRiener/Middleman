e9b831a87596f2f238124f68263e86eeb885c618
MatthiasRiener <matti.riener@gmail.com>
2021-10-04 17:25:59 +0200

    pricing section working on click

--- src/server/app/files/static/profile_settings/js/script.js
+++ src/server/app/files/static/profile_settings/js/script.js
@@ -49,3 +49,92 @@ $('body').on('click', '#sidebar-item-my-language', function () {
         $(this).children(".flag-image").css('background-image', 'url(' + imageUrl + ')');
     })
 });
+
+// pricing section
+
+var options = {
+    "no_pricelimit": {
+        "free": false,
+        "lite": true,
+        "standard": true,
+        "pro": true,
+    },
+    "no_monthly_trade_limit": {
+        "free": false,
+        "lite": true,
+        "standard": true,
+        "pro": true,
+    },
+    "custom_status": {
+        "free": false,
+        "lite": true,
+        "standard": true,
+        "pro": true,
+    },
+    "parallel_trading": {
+        "free": false,
+        "lite": false,
+        "standard": true,
+        "pro": true,
+    },
+    "recommended_by_platform": {
+        "free": false,
+        "lite": false,
+        "standard": false,
+        "pro": true,
+    },
+    "grab_open_offers": {
+        "free": false,
+        "lite": false,
+        "standard": false,
+        "pro": true,
+    },
+    "analytics_platform": {
+        "free": false,
+        "lite": false,
+        "standard": false,
+        "pro": true,
+    },
+};
+
+
+
+$('body').on('click', '.subscription-package-check', function () {
+    var el = $(this);
+    $('.subscription-packages-item').removeClass("selected-package");
+    el.closest(".subscription-packages-item").addClass("selected-package");
+
+    var id = el.closest(".subscription-packages-item")[0].id.replace('-package', '');
+
+    loadFeaturesById(id);
+
+})
+
+
+$('body').on('click', '#sidebar-item-my-subscriptions', function() {
+    loadFeaturesById("free");
+});
+
+
+function loadFeaturesById(id) {
+    $('.subscription-feature-checkbox').removeClass("ticked");
+    $('.subscription-feature-checkbox').removeClass("unticked");
+
+
+    $.each(options, function (key, value) {
+        var version_allowed = value[id];
+        console.log(key, version_allowed)
+
+        var element = $('#' + key).children(".subscription-feature-checkbox");
+        console.log(element)
+
+        if (version_allowed) {
+            element.addClass("ticked");
+            element.html(`<ion-icon name="checkmark"></ion-icon>`);
+        } else {
+            element.addClass("unticked");
+            element.html(`<ion-icon name="close"></ion-icon>`);
+        }
+
+    });
+}
